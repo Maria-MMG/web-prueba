cc474836d21158866584e3698659cd8f01e821f2
Maria-MMG <maria.jeevas@gmail.com>
2021-10-05 11:36:47 +0200

    creada la eb inicil

---
+++ Principal.java
@@ -0,0 +1,17 @@
+package dos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int contador = 1;
+		while (contador <= 10000000) {
+			System.out.println(contador);
+			contador = contador + 1;
+
+		}
+
+	}
+
+}
---
+++ Util.java
@@ -0,0 +1,13 @@
+public class Util {
+	String convertirAMayusculas(String texto) {
+		String textoEnMayusculas = texto.toUpperCase();
+		return textoEnMayusculas;
+		
+	}
+	
+	char obtenerPrimeraLetra(String texto) {
+		char primeraLetra = texto.charAt (0);
+		return primeraLetra;
+		
+	}
+}
